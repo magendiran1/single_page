90751f6abe6ef85693f6e77fef9fe241425015e3
Magendiran R <magendiran.r@ivygdcad.ivy-support.com>
2021-03-27 22:49:57 +0530

    css added

--- src/Component/Style.js
+++ src/Component/Style.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import '../App.css'
+import Radium from 'radium';
 const StyleUse = () => {
- 
+
 
 
 
@@ -23,30 +24,29 @@ const StyleUse = () => {
     //const [show ,setShow]=useState(listPersons.dataVisible)
 
     const dataUpdateList = () => {
-      let test =state.listPersons.students
-      test=[
-        { name: "magemendy", age: 25 },
-        { name: "Girishspidey", age: 24 },
-        { name: "Chackschandru", age: 30 }
-    ]
-        setState({
-            
-            ...state,
-            listPersons: test
-            
-        })
+        debugger;
+        const test = {...state}
+        test.listPersons.students = [
+            { name: "magemendy", age: 25 },
+            { name: "Girishspidey", age: 24 },
+            { name: "Chackschandru", age: 30 }
+        ]
+
+        setState(
+            test
+        )
     }
 
 
     const visibleDate = () => {
         let test1 = state.listPersons
-        test1.dataVisible=!state.listPersons.dataVisible
-       setState({
-          ...state,listPersons:test1
-         
-       })
+        test1.dataVisible = !state.listPersons.dataVisible
+        setState({
+            ...state, listPersons: test1
+
+        })
     }
-  
+
     const removeList = (name) => {
 
         const removeListItem = state.listPersons.students;
@@ -56,50 +56,68 @@ const StyleUse = () => {
 
         )
         let test2 = state.listPersons
-        test2.students=finalListItem
+        test2.students = finalListItem
 
         setState({
-            ...state,listPersons:test2
+            ...state, listPersons: test2
         });
     }
 
 
 
 
-    const listItem = state.listPersons.students.map((stdList, index) => {
+    const listItem =state.listPersons.students.map((stdList, index) => {
         return (<div key={index}><h1>My name is {stdList.name} my age is {stdList.age}
         </h1><button onClick={() => removeList(stdList.name)}>Delete</button></div>)
     })
 
-    const style ={
-        backgroundColor:'red',
-        color :'white',
+    const style = {
+        backgroundColor: 'red',
+        color: 'white',
         font: 'inherit',
         border: '1px solid bule',
         padding: '8px',
-        cursor: 'pointer'
+        cursor: 'pointer',
+        ':hover':{
+            backgroundColor:'lightgreen',
+            color:'red'
+        }
 
+ 
     }
-    const dataVisible=state.listPersons.dataVisible
+    const dataVisible = state.listPersons.dataVisible
     let show = null
-if(dataVisible){
-show=(<p>{listItem}</p>)
-style.backgroundColor='green'
+    if (dataVisible) {
+        show = (<p>{listItem}</p>)
+        style.backgroundColor = 'green'
+        style[":hover"]={
+            backgroundColor:'white',
+            color:'black'
+        }
+    }
+let classes =''
+
+
+if(state.listPersons.students.length<=2)
+{
+classes='red';
 }
-
-let classes =['red','blue'].join(' ');
-
-console.log(classes)
+if(state.listPersons.students.length>=3){
+    classes='blue';
+}
     return (
-        
+
         <div>
-<p className={classes}> This is Working Fine</p>
-            <button style={style} onClick={()=>visibleDate()} >Show The List</button> <br />
-            <button onClick={()=>dataUpdateList()}>updateList</button>
-          {show}
+            <p className={classes}> This is Working Fine</p>
+            <button style={style} onClick={() => visibleDate()} >Show The List</button> <br />
+            <button onClick={() => dataUpdateList()}>updateList</button>
+            {show}
 
         </div>
     )
 }
 
-export default StyleUse;
+export default Radium(StyleUse);
+
+
+/// we are going to use Radium in Style compentent the radium in external package  --- npm install --save radiu
